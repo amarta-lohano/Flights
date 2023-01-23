79aa5a8f5b1b6775d07cfe6f1baa21fb6e193f82
AmartaLohano <amarta.lohano@gmail.com>
2023-01-23 13:38:26 +1000

    Add project files.

---
+++ Flights/ReadModels/FlightRm.cs
@@ -0,0 +1,14 @@
+﻿namespace Flights.ReadModels
+{
+    public record FlightRm(
+        
+        Guid Id,
+        string airline,
+        string Price,
+        TimePlaceRm Departure,
+        TimePlaceRm Arrival,
+        int RemainingNumberOfSeats
+        );
+
+   
+}
